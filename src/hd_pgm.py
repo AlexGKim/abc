5048c0960afe8cb78600d756e41ae5d63c1dd069
Alex Kim <akim@tibia.dhcp.lbl.gov>
2015-07-02 09:17:08 -0700

    version given locally

--- src/hd_pgm.py
+++ src/hd_pgm.py
@@ -6,31 +6,38 @@ rc("font", family="serif", size=10)
 rc("text", usetex=True)
 
 
-pgm = PGM([7.5, 6.5], origin=[0., 0.2], observed_style='inner')
+pgm = PGM([9.5, 8.5], origin=[0., 0.2], observed_style='inner')
 
-pgm.add_node(Node('HD',r"$\vec{\mu},\vec{z}$, $\vec{\mathit{RA/Dec}}$", 1,4,scale=2))
-pgm.add_node(Node('theta_T',r"$\theta_T$", 1,6))
-pgm.add_node(Node('theta_G',r"$\theta_G$", 1,2))
+pgm.add_node(Node('HD',r"$\vec{\mu},\vec{z}$, $\vec{\mathit{RA/Dec}}$", 1,3,scale=2,aspect=1.2))
+pgm.add_node(Node('theta_T',r"$\theta_T$", 2,8))
+pgm.add_node(Node('theta_G',r"$\theta_G$", 2,1))
 
-pgm.add_node(Node('theta_Ti',r"$\theta_{Ti}$", 2,5))
-pgm.add_node(Node('theta_Gi',r"$\theta_{Gi}$", 2,3))
+pgm.add_node(Node('theta_Ti',r"$\theta_{Ti}$", 3,6))
+pgm.add_node(Node('theta_Gi',r"$\theta_{Gi}$", 3,4))
 
 
 pgm.add_node(Node('TypeHost',r"$T_i$, $G_i$", 2, 4, scale=1.5))
-pgm.add_node(Node('Flux',r"$n_i(t,\lambda)$", 2, 6, scale=1.2))
-pgm.add_node(Node('Flux_g',r"$n_{gi}(\lambda)$", 2, 2))
-pgm.add_node(Node('Transmission',r"$\phi(\lambda)$", 6, 4))
-pgm.add_node(Node('Counts',r"$\mathit{ADU}_i$", 3, 5,scale=1.2))
-pgm.add_node(Node('Counts_g',r"$\mathit{ADU}_{gi}$", 3, 1,scale=1.2))
-pgm.add_node(Node('Zeropoints',r"$\hat{Z}$", 6, 5, observed=True))
-pgm.add_node(Node('Counts_V',r"$\mathit{ADU}_{Vi}$", 3, 4, scale=1.2))
-pgm.add_node(Node('^Counts',r"$\hat{\mathit{ADU}_i}$,$\hat{\mathit{ADU}_{gi}}$", 5, 4, observed=True,scale=2))
+pgm.add_node(Node('Flux',r"$n_i(t,\lambda)$", 3, 7, scale=1.2))
+pgm.add_node(Node('Flux_g',r"$n_{gi}(\lambda)$", 3, 3))
+pgm.add_node(Node('Transmission',r"$\phi(\lambda)$", 4, 8))
+pgm.add_node(Node('Counts',r"$\mathit{ADU}_i$", 5, 6,scale=1.2))
+pgm.add_node(Node('Counts_g',r"$\mathit{ADU}_{gi}$", 5, 3,scale=1.2))
+pgm.add_node(Node('Zeropoints',r"$\hat{Z}$", 6, 8, observed=True))
+pgm.add_node(Node('Counts_V',r"$\mathit{ADU}_{Vi}$", 5, 5, scale=1.3))
+pgm.add_node(Node('^Counts',r"$\hat{\mathit{ADU}_i}$,$\hat{\mathit{ADU}_{gi}}$", 7, 4, observed=True,scale=2,aspect=1.2))
 
-pgm.add_node(Node('Spars',r"$\hat{T}_{Si}$, $\hat{z}_{Si},\hat{\theta}_{Si}$", 4, 6, scale=2.1,observed=True))
-pgm.add_node(Node('^Host',r"$\hat{z}_{Hi},\hat{\theta}_{Hi}$", 2, 1, scale=1.5,observed=True))
-pgm.add_node(Node('Galaxies',r"$\hat{\mathit{Gals.}}$", 1, 1, observed=True))
+pgm.add_node(Node('Spars',r"$\hat{T}_{Si}$, $\hat{z}_{Si},\hat{\theta}_{Si}$", 7, 7, scale=1.8,aspect=1.2, observed=True))
+pgm.add_node(Node('^Host',r"$\hat{z}_{Hi},\hat{\theta}_{Hi}$", 7, 2, scale=1.5,observed=True))
+pgm.add_node(Node('Galaxies',r"$\hat{\mathit{Gals.}}$", 6, 1, observed=True))
 
-pgm.add_node(Node('Detected',r"$\hat{D}_i$", 4, 3, observed=True))
+pgm.add_node(Node('Detected',r"$\hat{D}_i$", 6, 4, observed=True))
+
+pgm.add_node(Node('^Type',r"$\hat{T}_i$", 8, 4, observed=True))
+
+pgm.add_edge("Spars","^Type")
+pgm.add_edge("^Host","^Type")
+pgm.add_edge("^Counts","^Type")
+
 
 pgm.add_edge("HD","TypeHost")
 pgm.add_edge("HD","Flux")
@@ -40,6 +47,9 @@ pgm.add_edge("HD","^Host")
 
 pgm.add_edge("TypeHost","theta_Ti")
 pgm.add_edge("TypeHost","theta_Gi")
+pgm.add_edge("TypeHost","theta_T")
+pgm.add_edge("TypeHost","theta_G")
+
 
 pgm.add_edge("theta_T","Flux")
 pgm.add_edge("theta_G","Flux_g")
@@ -102,7 +112,7 @@ pgm.add_edge("Detected","^Host")
 
 
 # Big Plate: Galaxy
-pgm.add_plate(Plate([1.5, 0.5, 4, 6.],
+pgm.add_plate(Plate([1.5, 1.5, 7, 6.],
                     label=r"SNe $\i = 1, \cdots, N_{SN}$",
                     shift=-0.1))
 
